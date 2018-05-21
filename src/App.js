0e1605e3e308742c321dbb7689ab5915828d542d
Matthew Zielke <MZielke@buxtonco.com>
2018-05-21 14:14:19 -0500

    added linkedin logo & link in footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import {Link, Route, Redirect} from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
 import Home from './containers/Home/Home';
+import Work from './containers/Work/Work';
+import LinkedInLogo from './assets/In-2C-28px-R.png';
+
+const linkedInLogoPadding = {
+  padding: '2px'
+};
 
 const App = () => (
+
   <div>
     <Redirect from="/" to="/Home"/>
+    {/* toolbar */}
     <nav>
       <Link to="/Home">Home</Link>
+      <Link to="/Work">Work</Link>
     </nav>
     <div>
       <Route path="/Home" component={Home}/>
+      <Route path="/Work" component={Work}/>
+    </div>
+    {/* footer */}
+    <div>
+      <a href="https://www.linkedin.com/in/matthew-zielke-a8a607128/" target="_blank" rel="noopener noreferrer">
+        <img style={linkedInLogoPadding} src={LinkedInLogo} alt="Go to my LinkedIn Profile!" width="37" height="28" border="0"/>
+      </a>
     </div>
   </div>
 )
--- src/containers/Work/Work.js
+++ src/containers/Work/Work.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
-import '../../App.css';
 
-const Home = () => (
+const Work = () => (
     <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome</h1>
+            <h1 className="App-title">Work</h1>
         </header>
         <p className="App-sub-header">
             My Site is Currently Under Construction Please Check Back at a Later Date
@@ -14,4 +11,4 @@ const Home = () => (
     </div>
 )
 
-export default Home;
+export default Work;
