775cf7e6b5e7a32696bd32e00525a6c6e040638f
Matthew Zielke <MZielke@buxtonco.com>
2018-05-31 15:36:48 -0500

    toned down amount of colors showing at once

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import LinkedInLogo from '../../assets/In-2C-28px-R.png';
 import Aux from '../../hoc/Auxx/Auxx';
+import './Footer.css';
 
 const linkedInLogoPadding = {
-    padding: '2px'
+    padding: '12px'
 };
 
 const Footer = () => (
     <Aux>
-        <a href="https://www.linkedin.com/in/matthew-zielke-a8a607128/" target="_blank" rel="noopener noreferrer">
-            <img style={linkedInLogoPadding} src={LinkedInLogo} alt="Go to my LinkedIn Profile!" width="37" height="28" border="0"/>
-        </a>
+        <div className="footer">
+            <div>Find me on LinkedIn</div>
+            <a href="https://www.linkedin.com/in/matthew-zielke-a8a607128/" target="_blank" rel="noopener noreferrer">
+                <img style={linkedInLogoPadding} src={LinkedInLogo} alt="Go to my LinkedIn Profile!" width="37" height="28" border="0"/>
+            </a>
+        </div>
     </Aux>
 )
 
