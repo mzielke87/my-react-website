4e39a443f429e1d033b41d8885672b949013bf7f
Matthew Zielke <MZielke@buxtonco.com>
2018-06-12 15:33:19 -0500

    fixed styling inside infocard and added some info to project iris

--- src/components/Toolbar/Projects/Projects.js
+++ src/components/Toolbar/Projects/Projects.js
@@ -13,7 +13,10 @@ const Projects = () => (
         </p>
         <div className="App-body">
             {/* include links to all work on github with description for each */}
-            <InfoCard title={'Project Iris'} subheader={'Gaze tracking software'} body={''} link={'https://github.com/mzielke87/Project-Iris'}/>
+            <InfoCard title={'Project Iris'} 
+                        subheader={'Gaze tracking software'} 
+                        body={'This was my senior design project while attending UTA. Me and 3 other students developed this software to work in conjunction with an Intel RealSense SR300 camera to effectively track your gaze position on a computer screen as well as use that gaze position to help you select letters with a cursor on the screen to "type" hands free.'} 
+                        link={'https://github.com/mzielke87/Project-Iris'}/>
         </div>
     </div>
 )
