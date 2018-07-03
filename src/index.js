813ace1bfe7571a2f8b14e4bbc7ae4569c01f8ad
Matthew Zielke <MZielke@buxtonco.com>
2018-07-03 17:45:44 -0500

    well that didnt work, reverting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import unregisterServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
@@ -10,4 +10,4 @@ ReactDOM.render((
     </BrowserRouter>
 ), document.getElementById('root'));
 
-unregisterServiceWorker();
+registerServiceWorker();
