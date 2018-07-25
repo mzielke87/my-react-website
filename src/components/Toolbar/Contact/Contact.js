47ad04bebac641f96769345b3e2e5a2d8ec5842c
Matthew Zielke <MZielke@buxtonco.com>
2018-07-25 08:42:09 -0500

    added profile pic and text to contact card

--- src/components/Toolbar/Contact/Contact.js
+++ src/components/Toolbar/Contact/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './Contact.css';
 import '../../../App.css';
+import myProfile from '../../../assets/profilecircle.png';
 
 const Contact = () => (
     <div className="App">
@@ -11,7 +12,12 @@ const Contact = () => (
         <p className="App-sub-header"></p>
         <div className="App-body">
             <div className='contact-card'>
-                <div>In progress!</div>
+                <div className="contact-profile-pic-container">
+                    <img className="contact-profile-pic" src={myProfile} alt="myProfile" />
+                </div>
+                <div className="contact-card-body">
+                    Please use the links below to contact me. Send me an email or check out my LinkedIn and send me a message there.
+                </div>
             </div>
         </div>
     </div>
