62ff5d22482931ae4c77768b21699773bb7c6bce
Matthew Zielke <MZielke@buxtonco.com>
2018-07-26 08:56:24 -0500

    rewording in about me

--- src/App.js
+++ src/App.js
@@ -53,12 +53,13 @@ class App extends Component {
           title='Matthew Zielke'
           from='left'
           onRequestClose={() => this.setState({isPaneOpen: false})}>
+          <h1>Hello!</h1>
+          <br/>
           <p>I am Software Engineer with experience in web development.</p>
             <br/>
             <p>I am always open to a project that will help further my knowledge.</p>
             <br/>
-            <p>Currently I am working as a Full Stack Software Engineer at Buxton Co in Fort Worth, Texas.
-                 Please use the links below to reach out to me if you would like to work together.</p>
+            <p>Currently I am working as a Full Stack Software Engineer at Buxton Co in Fort Worth, Texas.</p>
             <br/>
         </SlidingPane>
         <Footer />
